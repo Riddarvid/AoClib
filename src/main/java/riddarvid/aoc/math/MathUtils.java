6e2cbf2d3a012d4bbd70fc06dab268366ef50397
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2020-12-03 08:28:56 +0100

    Add product

--- src/main/java/riddarvid/aoc/math/MathUtils.java
+++ src/main/java/riddarvid/aoc/math/MathUtils.java
@@ -1,7 +1,17 @@
 package riddarvid.aoc.math;
 
+import java.util.List;
+
 public class MathUtils {
     public static boolean inRange(int low, int high, int n) {
         return n >= low && n <= high;
+    }
+
+    public static long product(List<Integer> ns) {
+        long product = 1;
+        for (int n : ns) {
+            product *= n;
+        }
+        return product;
     }
 }
