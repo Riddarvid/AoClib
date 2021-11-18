2be93fa04929db88af3fa7b1f522da174df7f0e1
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2021-11-18 11:25:40 +0100

    Change structure

--- src/main/java/riddarvid/aoc/printing/PrintUtils.java
+++
@@ -1,9 +0,0 @@
-package riddarvid.aoc.printing;
-
-public class PrintUtils {
-    public static void printCharMatrix(char[][] matrix) {
-        for (char[] chars : matrix) {
-            System.out.println(new String(chars));
-        }
-    }
-}
